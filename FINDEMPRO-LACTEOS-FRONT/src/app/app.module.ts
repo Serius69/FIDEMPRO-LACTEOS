e69bd9d40e234a58dc93213eb1cb0a3cd606c437
Serius69 <seriogionis@gmail.com>
2023-08-23 15:00:12 -0400

    Agregacion de auth

--- FINDEMPRO-LACTEOS-FRONT/src/app/app.module.ts
+++ FINDEMPRO-LACTEOS-FRONT/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/auth/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { TestComponent } from './components/test/test.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
 import { Routes, RouterModule, Router} from '@angular/router';
 import myAppConfig from './config/my-app-config';
 
@@ -15,6 +15,7 @@ import { routing } from "./app.routing";
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { SettingsThemeComponent } from './components/settings-theme/settings-theme.component';
+import { InstructionsComponent } from './components/instructions/instructions.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { SettingsThemeComponent } from './components/settings-theme/settings-the
     SignupComponent,
     FooterComponent,
     MenuComponent,
-    SettingsThemeComponent
+    SettingsThemeComponent,
+    InstructionsComponent
   ],
   imports: [BrowserModule, routing],
   providers: [],
--- FINDEMPRO-LACTEOS-FRONT/src/app/app.routing.ts
+++ FINDEMPRO-LACTEOS-FRONT/src/app/app.routing.ts
@@ -2,10 +2,11 @@
 
 import { RouterModule } from "@angular/router";
 import { AppComponent } from "./app.component";
-import { LoginComponent } from "./components/login/login.component";
-import { SignupComponent } from "./components/signup/signup.component";
+import { LoginComponent } from "./components/auth/login/login.component";
+import { SignupComponent } from "./components/auth/signup/signup.component";
 import { HomeComponent } from "./components/home/home.component";
 import { TestComponent } from "./components/test/test.component";
+import { InstructionsComponent } from "./components/instructions/instructions.component";
 
 const appRoutes = [
   { path: "", component: AppComponent, pathMatch: "full" },
@@ -13,7 +14,7 @@ const appRoutes = [
   { path: "signup", component: SignupComponent, pathMatch: "full" },
   { path: "home", component: HomeComponent, pathMatch: "full" },
   { path: "test", component: TestComponent, pathMatch: "full" },
-  { path: "home", component: HomeComponent, pathMatch: "full" },
+  { path: "instructions", component: InstructionsComponent, pathMatch: "full" },
   { path: "home", component: HomeComponent, pathMatch: "full" },
 ];
 export const routing = RouterModule.forRoot(appRoutes);
