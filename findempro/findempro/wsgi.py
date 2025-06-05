05440d9549c7b815010a04710f424dd509207820
Serius69 <seriogionis@gmail.com>
2025-06-05 08:56:20 -0400

    CI/CD 1

--- findempro/findempro/wsgi.py
+++ findempro/findempro/wsgi.py
@@ -1,16 +1,10 @@
 """
 WSGI config for findempro project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'findempro.settings')
 
-application = get_wsgi_application()
+# Para Vercel
+app = get_wsgi_application()
